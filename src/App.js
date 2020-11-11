d5e7721f76f16a99759786f12a6a1126942f4b69
Marcel Cornett <marcornett@gmail.com>
2020-11-10 20:13:11 -0500

    Created form and reorganized input tags. Submission method added. Event input tag with handlechange method.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import moment from 'moment'
 import Timer from './Timer'
 
 function App() {
-  const [currTime, setTime] = useState()
+  const [currTime, setTime] = useState('')
+  const [event, setEvent] = useState('')
+
   const current = new Date()
-  const date = `${current.getMonth()}/${current.getDate()}/${current.getFullYear()}`
-  
-  useEffect(()=>{
-    setInterval(()=>{
+
+  useEffect(() => {
+    setInterval(() => {
       setTime(new Date().toLocaleString())
     }, 1000)
-  },[])
-  
-  
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const then = moment()
+  }
+
+  const handleEventChange = (e) => {
+    setEvent(e.target.value)
+  }
+
   return (
     <div className="App">
-      <span>Event:</span>
-      <input type="text"/>
-      <input type="date" name="" id=""/>
-      <span>Optional time:</span>
-      <input type="text"/>
-      <button>Start</button>
-      <div>Date: {date}</div>
+      <form action="" onSubmit={handleSubmit}>
+
+        <span>Event: </span>
+        <input type="text" onChange={handleEventChange} value={event} />
+        <p></p>
+
+        <span>Date: </span>
+        <input type="date" name="" id="" />
+        <p></p>
+
+        <span>Optional time: </span>
+        <input type="text" />
+
+        <p><input type="submit" value="Start" /></p>
+      </form>
+
       <div>Time: {currTime}</div>
     </div>
   );
