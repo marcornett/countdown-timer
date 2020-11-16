2e64b5493afeef377340de6184c11b0ff4a701a2
Marcel Cornett <marcornett@gmail.com>
2020-11-15 20:08:15 -0500

    Moved timer display to a component through props.

--- src/Timer.js
+++ src/Timer.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Timer.css'
 
-function Timer() {
+function Timer({days, hours, minutes, seconds, event}) {
     return (
         <div>
-            Timer
+            <h4>{event}</h4>
+            <h2>{days || 0} days</h2>
+            <h2>{hours || 0} hours</h2>
+            <h2>{minutes || 0} minutes</h2>
+            <h2>{seconds || 0} seconds</h2>
         </div>
     )
 }
